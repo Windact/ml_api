d981119233c81b548976b2232f75707d1fadb7d6
Windact <52325204+Windact@users.noreply.github.com>
2021-06-09 00:47:26 +0200

    Updating the config and the api with OpenAPI spec

--- api/config.py
+++ api/config.py
@@ -17,6 +17,30 @@ UPLOAD_FOLDER.mkdir(exist_ok=True)
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
+class Config:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SERVER_PORT = 5000
+    UPLOAD_FOLDER = UPLOAD_FOLDER
+    SERVER_ADDRESS= os.environ.get('SERVER_ADDRESS', '0.0.0.0')
+    SERVER_PORT= os.environ.get('SERVER_PORT', '5000')
+    LOGGING_LEVEL = os.environ.get("LOGGING_LEVEL", logging.INFO)
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SERVER_ADDRESS= os.environ.get('SERVER_ADDRESS', '0.0.0.0')
+    SERVER_PORT= os.environ.get('SERVER_PORT', '5000')
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    LOGGING_LEVEL = logging.DEBUG
+
+class TestingConfig(Config):
+    TESTING = True
+    LOGGING_LEVEL = logging.DEBUG
 
 def get_console_handler():
     console_handler = logging.StreamHandler(sys.stdout)
@@ -45,28 +69,28 @@ def get_logger(*, logger_name):
 
     return logger
 
-
-class Config:
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    SERVER_PORT = 5000
-    UPLOAD_FOLDER = UPLOAD_FOLDER
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-    SERVER_ADDRESS: os.environ.get('SERVER_ADDRESS', '0.0.0.0')
-    SERVER_PORT: os.environ.get('SERVER_PORT', '5000')
+def setup_app_logging(config: Config) -> None:
+    """Prepare custom logging for our application."""
+    _disable_irrelevant_loggers()
+    root = logging.getLogger()
+    root.setLevel(config.LOGGING_LEVEL)
+    root.addHandler(get_console_handler())
+    root.propagate = False
 
 
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
+def _disable_irrelevant_loggers() -> None:
+    """Disable loggers created by packages which create a lot of noise."""
+    for logger_name in (
+        "connexion.apis.flask_api",
+        "connexion.apis.abstract",
+        "connexion.decorators",
+        "connexion.operation",
+        "connexion.operations",
+        "connexion.app",
+        "openapi_spec_validator",
+    ):
+        logging.getLogger(logger_name).level = logging.WARNING
 
 
-class TestingConfig(Config):
-    TESTING = True
 
 
